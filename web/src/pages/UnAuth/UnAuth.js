1e4c41dcf1b5f4a8a2427337b3132b8fbd346621
u1105372 <vaitheeswaran.p@iqvia.com>
2021-12-02 15:40:27 +0530

    removed error

--- web/src/pages/UnAuth/UnAuth.js
+++ web/src/pages/UnAuth/UnAuth.js
@@ -5,11 +5,12 @@ const UnAuth = () => {
     let history = useHistory();
     useEffect(() => {
         window.location.href = `${process.env.REACT_APP_LAUNCH_URL}`;
+        if(window.location.href.startsWith('https://dev2-fedsvc.solutions.iqvia.com/authenticationendpoint')){
+            window.location.href = 'http://localhost:3000/oauth2client';
+        }
     }, [])
 
-    if(window.location.href.startsWith('https://dev2-fedsvc.solutions.iqvia.com/authenticationendpoint'){
-        window.location.href == 'http://localhost:3000/oauth2client';
-    }
+
 
     return (
         <div>
