b50e71a6021094523694ea927542e4e1e0b57423
Gurpreet Singh <gurpreet.singh@iqvia.com>
2021-12-09 20:12:36 +0530

    Modal changes

--- web/src/components/AddStudyModal/AddStudyModal.js
+++ web/src/components/AddStudyModal/AddStudyModal.js
@@ -3,6 +3,8 @@ import Modal from 'apollo-react/components/Modal';
 import { useState } from "react";
 import './AddStudyModal.scss';
 import { useEffect } from "react";
+import Typography from "apollo-react/components/Typography";
+import Search from 'apollo-react/components/Search';
 
 const AddStudyModal = ({
     history,
@@ -19,17 +21,22 @@ const AddStudyModal = ({
         setOpenModal(open);
       }, [open]);
   return (
-    <div style={modalWrapper}>
+    <>
             <Modal
             open={openModal}
             onClose={handleClose}
             title="Add New Study"
             buttonProps={[{}, { label: 'Next' }]}
             id="addStudyModal"
+            className="custom-modal"
         >
+          <div className="modal-content">
+          <Typography variant="caption">Search for a study</Typography>
+          <Search placeholder="Search" fullWidth />
           <h2>Hello</h2>
+          </div>
           </Modal>
-    </div>
+    </>
   );
 };
 
