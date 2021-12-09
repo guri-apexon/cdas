4b535969656600d7f7b796922a4e9418d2efd454
u1105372 <vaitheeswaran.p@iqvia.com>
2021-12-09 14:26:21 +0530

    added log file into gitignore

--- server/src/config/logger.js
+++ server/src/config/logger.js
@@ -4,7 +4,7 @@ const { combine, timestamp, json } = format;
 const logger = createLogger({
     transports: [
         new transports.Console(),
-        new transports.File({ filename: 'sat-server.log' })
+        new transports.File({ filename: 'cdas-server.log' })
       ],
       format: combine(
         timestamp({
