f4077282504925b9b09ebe6a477bc970a9ef27b5
u1105372 <vaitheeswaran.p@iqvia.com>
2021-12-17 17:19:18 +0530

    updates

--- server/src/controller/StudyController.js
+++ server/src/controller/StudyController.js
@@ -79,38 +79,38 @@ exports.getStudyList = async (req, res) => {
   }
 };
 
-exports.getUniqueValueofColumn = async (req, res) => {
-  try {
-    let searchData = req.body;
-    let { columnName } = searchData;
-    //   searchData;
-    // const ist_update = new Date();
+// exports.getUniqueValueofColumn = async (req, res) => {
+//   try {
+//     let searchData = req.body;
+//     let { columnName } = searchData;
+//     //   searchData;
+//     // const ist_update = new Date();
 
-    // const searchString = searchData.searchString ? searchData.searchString : "";
-    // const searchStringDataType = searchData.searchStringDataType
-    //   ? searchData.searchStringDataType
-    //   : "";
+//     // const searchString = searchData.searchString ? searchData.searchString : "";
+//     // const searchStringDataType = searchData.searchStringDataType
+//     //   ? searchData.searchStringDataType
+//     //   : "";
 
-    // const offset = pageNo > 1 ? (pageNo - 1) * pageLimit : 0;
-    // const query =
-    //   "SELECT prot_nbr as protocolNumber, spnsr_nm as sponsorName, phase as phase, prot_status as protocolStatus, thptc_area as therapeuticArea, project_code as projectCode from cdascore1d.cdascore.cdas_study_master";
+//     // const offset = pageNo > 1 ? (pageNo - 1) * pageLimit : 0;
+//     // const query =
+//     //   "SELECT prot_nbr as protocolNumber, spnsr_nm as sponsorName, phase as phase, prot_status as protocolStatus, thptc_area as therapeuticArea, project_code as projectCode from cdascore1d.cdascore.cdas_study_master";
 
-    const query = "SELECT prot_nbr as protocolnumber, spnsr_nm as sponsorname, phase, prot_stat as protocolstatus, cs.insrt_tm as dateadded, cs.updt_tm as dateedited, ob_stat as onboardingprogress, cs.usr_descr as assignmentcount, thptc_area as therapeuticarea, proj_cd as projectcode FROM cdascore.cdas_study cs INNER JOIN cdascore.cdas_sponsor cs2 ON cs2.spnsr_id = cs.spnsr_id ORDER BY cs.insrt_tm";
+//     const query = "SELECT DISTINCT  FROM cdascore.cdas_study";
 
-    Logger.info({
-      message: "getStudyList",
-    });
+//     Logger.info({
+//       message: "getStudyList",
+//     });
 
-    const $data = await DB.executeQuery(query);
-    return apiResponse.successResponseWithData(
-      res,
-      "Operation success",
-      $data.rows
-    );
-  } catch (err) {
-    //throw error in json response with status 500.
-    Logger.error("catch :studyList");
-    Logger.error(err);
-    return apiResponse.ErrorResponse(res, err);
-  }
-};
+//     const $data = await DB.executeQuery(query);
+//     return apiResponse.successResponseWithData(
+//       res,
+//       "Operation success",
+//       $data.rows
+//     );
+//   } catch (err) {
+//     //throw error in json response with status 500.
+//     Logger.error("catch :studyList");
+//     Logger.error(err);
+//     return apiResponse.ErrorResponse(res, err);
+//   }
+// };
