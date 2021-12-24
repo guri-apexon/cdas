69dd9e3316e9f8c722c140440bcd92f801619dbe
Gurpreet Singh <gurpreet.singh@iqvia.com>
2021-12-24 14:00:15 +0530

    Test changes

--- web/src/components/AddStudyModal/AddStudyModal.test.js
+++ web/src/components/AddStudyModal/AddStudyModal.test.js
@@ -2,37 +2,30 @@ import { cleanup, render, fireEvent, screen } from "@testing-library/react";
 import { createMemoryHistory } from "history";
 import { Router } from "react-router-dom";
 import AddStudyModal from "./AddStudyModal";
-
-// describe("Flatten tests:", () => {
-//   it("Flattens an array.", () => {
-//     const nestedArray = [[1, 2, 3], [4]];
-//     const flatArray = [1, 2, 3, 4];
-//     expect(flatten(nestedArray)).toEqual(flatArray);
-//   });
-//   it("Returns empty array when the input is an empty array.", () => {
-//     const array = [];
-//     const result = flatten(array);
-//     const expectedResult = [];
-//     expect(result).toEqual(expectedResult);
-//   });
-// });
+import StudyNotOnboarded from "../../pages/StudySetup/StudyNotOnboarded";
 
 afterEach(cleanup);
 
 // testing a controlled component form.
 it("Inputing text updates the state", () => {
-// const { getByText, getByPlaceholderText } = render(<AddStudyModal />);
   const history = createMemoryHistory();
-  render(
+  const {container} = render(
     <Router history={history}>
-      <AddStudyModal />
+      <AddStudyModal open={true} onClose={() => console.log("Hello")} />
+      {/* <StudyNotOnboarded
+        studyData={{
+          notOnBoardedStudyStatus: { inprogress_count: 100, faliure_count: 100 },
+        }}
+        selectedStatus={''}
+        selectedFilter={''}
+      /> */}
     </Router>
   );
-  expect(screen.getByText(/Search for a study/i).textContent).toBe("Search for a study");
+  expect(container.querySelector(".custom-modal")).toBeInTheDocument();
 
 //   fireEvent.change(getByLabelText("Input Text:"), {
 //     target: { value: "Text" },
 //   });
-
 //   expect(getByText(/Change/i).textContent).not.toBe("Change: ");
+
 });
--- web/src/setupTests.js
+++ web/src/setupTests.js
@@ -6,5 +6,6 @@ import "@testing-library/jest-dom";
 
 import { configure } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import "@testing-library/jest-dom/extend-expect";
 
 configure({ adapter: new Adapter() });
