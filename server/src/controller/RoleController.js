9ec8ad3a43e3a6e734ba0848c34e5877bf27d5b3
Gurpreet Singh <gurpreet.singh@iqvia.com>
2022-02-08 20:03:10 +0530

    Create Role updates

--- server/src/controller/RoleController.js
+++ server/src/controller/RoleController.js
@@ -3,14 +3,15 @@ const apiResponse = require("../helpers/apiResponse");
 const Logger = require("../config/logger");
 const moment = require("moment");
 const constants = require("../config/constants");
+const messages = require("../config/messages");
 const helpers = require("../helpers/customFunctions");
 const { _ } = require("lodash");
 
 exports.createRole = function (req, res) {
   try {
     const { name, description, policies, userId, status } = req.body;
-    if(!policies?.length || !Array.isArray(policies)){
-      return apiResponse.ErrorResponse(res, "Please select atleast one policy to proceed.");
+    if(!policies?.length || !Array.isArray(policies) || !userId){
+      return apiResponse.ErrorResponse(res, "Please fill mandatory fields to proceed.");
       return false;
     }
     const currentTime = helpers.getCurrentTime();
@@ -28,28 +29,22 @@ exports.createRole = function (req, res) {
       roleValues
     ).then((response) => {
         const role = response.rows[0];
-        console.log("role", role);
-        const rolePolicyValues = [
-          role.role_id,
-          policyId,
-          1,
-          userId,
-          currentTime,
-          userId,
-          currentTime,
-        ];
-        DB.executeQuery(
-          `INSERT into ${constants.DB_SCHEMA_NAME}.role_policy(role_id, plcy_id, act_flg, created_by, created_on, updated_by, updated_on) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
-          roleValues
-        ).then((response) => {
-            console.log("response", response);
-          });
+        let queryStr='';
+        policies.forEach((policyId)=>{
+          queryStr += `INSERT into ${constants.DB_SCHEMA_NAME}.role_policy(role_id, plcy_id, act_flg, created_by, created_on, updated_by, updated_on) VALUES('${role.role_id}', '${policyId}', 1, '${userId}', '${currentTime}', '${userId}', '${currentTime}');`;
+        });
+        DB.executeQuery(queryStr).then((response) => {
+          return apiResponse.successResponseWithData(res, messages.CREATE_ROLE_SUCCESS, {});
+        }).catch(err=>{
+          return apiResponse.ErrorResponse(res, err?.detail);
+        });
       })
       .catch((err) => {
-        const errMessage = err.code==23505 ? "Role Name should be unique - Please update the name and Save again" : err.detail;
+        const errMessage = err.code==23505 ? messages.CREATE_ROLE_UNIQUE : err.detail;
         return apiResponse.ErrorResponse(res, errMessage);
       });
   } catch (err) {
+    console.log("Err", err);
     return apiResponse.ErrorResponse(res, err);
   }
 };
