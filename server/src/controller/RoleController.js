d84402fc1d396571097497634ffa3cda0e403dbc
u1118184 <akhil.kumar@iqvia.com>
2022-02-09 16:41:41 +0530

    changed query list roles

--- server/src/controller/RoleController.js
+++ server/src/controller/RoleController.js
@@ -10,8 +10,11 @@ const { _ } = require("lodash");
 exports.createRole = function (req, res) {
   try {
     const { name, description, policies, userId, status } = req.body;
-    if(!policies?.length || !Array.isArray(policies) || !userId){
-      return apiResponse.ErrorResponse(res, "Please complete all mandatory information and then click Save");
+    if (!policies?.length || !Array.isArray(policies) || !userId) {
+      return apiResponse.ErrorResponse(
+        res,
+        "Please complete all mandatory information and then click Save"
+      );
       return false;
     }
     const currentTime = helpers.getCurrentTime();
@@ -27,20 +30,28 @@ exports.createRole = function (req, res) {
     DB.executeQuery(
       `INSERT into ${constants.DB_SCHEMA_NAME}.role(role_nm, role_desc, role_stat, created_by, created_on, updated_by, updated_on) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
       roleValues
-    ).then((response) => {
+    )
+      .then((response) => {
         const role = response.rows[0];
-        let queryStr='';
-        policies.forEach((policyId)=>{
+        let queryStr = "";
+        policies.forEach((policyId) => {
           queryStr += `INSERT into ${constants.DB_SCHEMA_NAME}.role_policy(role_id, plcy_id, act_flg, created_by, created_on, updated_by, updated_on) VALUES('${role.role_id}', '${policyId}', 1, '${userId}', '${currentTime}', '${userId}', '${currentTime}');`;
         });
-        DB.executeQuery(queryStr).then((response) => {
-          return apiResponse.successResponseWithData(res, messages.CREATE_ROLE_SUCCESS, {});
-        }).catch(err=>{
-          return apiResponse.ErrorResponse(res, err?.detail);
-        });
+        DB.executeQuery(queryStr)
+          .then((response) => {
+            return apiResponse.successResponseWithData(
+              res,
+              messages.CREATE_ROLE_SUCCESS,
+              {}
+            );
+          })
+          .catch((err) => {
+            return apiResponse.ErrorResponse(res, err?.detail);
+          });
       })
       .catch((err) => {
-        const errMessage = err.code==23505 ? messages.CREATE_ROLE_UNIQUE : err.detail;
+        const errMessage =
+          err.code == 23505 ? messages.CREATE_ROLE_UNIQUE : err.detail;
         return apiResponse.ErrorResponse(res, errMessage);
       });
   } catch (err) {
@@ -55,12 +66,12 @@ exports.listRoles = async function (req, res) {
       message: "listRoles",
     });
     let q = `SELECT r.role_id,r.role_nm ,r.role_desc,r.role_stat,p2.prod_nm 
-    FROM cdascfg."role" r 
-    INNER JOIN cdascfg.role_policy rp 
+    FROM ${constants.DB_SCHEMA_NAME}."role" r 
+    FULL OUTER JOIN ${constants.DB_SCHEMA_NAME}.role_policy rp 
     ON r.role_id = rp.role_id
-    inner join cdascfg.policy_product pp 
+    FULL OUTER join ${constants.DB_SCHEMA_NAME}.policy_product pp 
     on pp.plcy_id = rp.plcy_id 
-    inner join cdascfg.product p2 
+    FULL OUTER join ${constants.DB_SCHEMA_NAME}.product p2 
     on p2.prod_id = pp.prod_id`;
     let { rows } = await DB.executeQuery(q);
     let tempRows = _.uniqBy(rows, "role_id");
