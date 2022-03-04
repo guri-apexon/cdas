56de0d2a8c906e040e6aeb352f29d65f22707910
u1105372 <vaitheeswaran.p@iqvia.com>
2022-03-04 15:28:59 +0530

    update for API

--- server/src/route/role.js
+++ server/src/route/role.js
@@ -7,5 +7,6 @@ router.post("/update", RoleController.updateRole);
 router.get("/", RoleController.listRoles);
 router.get("/:roleId", RoleController.getDetails);
 router.post("/update/status", RoleController.updateStatus);
+router.post("/getUserRolesPermissions", RoleController.getRolesPermissions);
 
 module.exports = router;
