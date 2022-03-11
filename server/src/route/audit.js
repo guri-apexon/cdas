a3767aaed33341842e2a0980bee66b494cbd8245
u1105372 <vaitheeswaran.p@iqvia.com>
2022-03-11 17:05:07 +0530

    removed audit

--- server/src/route/audit.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const AuditController = require("../controller/AuditController");
-
-var router = express.Router();
-
-router.post("/create", AuditController.createAuditLog);
-
-module.exports = router;
