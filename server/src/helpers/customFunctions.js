37adede95ef33c136a4d3561644a12a01ae322b7
Gurpreet Singh <gurpreet.singh@iqvia.com>
2022-03-16 17:32:11 +0530

    cookies update

--- server/src/helpers/customFunctions.js
+++ server/src/helpers/customFunctions.js
@@ -8,3 +8,11 @@ exports.createUniqueID = () => {
 exports.getCurrentTime = () => {
   return moment().utc().format("YYYY-MM-DD HH:mm:ss");
 };
+exports.getDomainWithoutSubdomain = url => {
+  const urlParts = new URL(url).hostname.split('.')
+
+  return urlParts
+    .slice(0)
+    .slice(-(urlParts.length === 4 ? 3 : 2))
+    .join('.')
+}
