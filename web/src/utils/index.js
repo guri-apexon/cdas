6e6de376441d981e24595d43ac159aac5acdc51a
Reddy <thatiparthisivasankar.reddy@iqvia.com>
2022-04-07 13:30:04 +0530

    fixed CDAS-10775

--- web/src/utils/index.js
+++ web/src/utils/index.js
@@ -41,7 +41,7 @@ export function getPathnameAndSearch(path) {
 }
 
 export function getLastLogin() {
-  const currentLogin = getCookie("user.last_login_ts");
+  const currentLogin = getCookie("user.current_login_ts");
   if (currentLogin === "first_time") return null;
   const localDate = moment.unix(currentLogin).local();
   return localDate.format("DD-MMM-YYYY hh:mm A");
