0288b00a69f62147374a0f05af084e417ccad2fe
Sama <vamsi.sama@iqvia.com>
2022-05-13 14:43:39 +0530

    changes with the dead code

--- web/src/generic.functions.js
+++ web/src/generic.functions.js
@@ -1,15 +1,7 @@
 // export function convertUTCToLocalString(date) {
 //   var newDate = new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000);
 
-//   var offset = date.getTimezoneOffset() / 60;
-//   var hours = date.getHours();
-
-//   newDate.setHours(hours - offset);
-
-//   return newDate;
-// }
-
-// convertUTCToLocalString("1994-11-05T13:15:30Z");
+// IS-8016 UTC format "1994-11-05T13:15:30Z"
 
 export function UTCToLocalString(date) {
   return new Date(date);
