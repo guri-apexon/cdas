ee4765bc670305b391a3e8c542c6d475da83bf3b
Sama <vamsi.sama@iqvia.com>
2022-05-30 12:18:38 +0530

    changes with the time format

--- web/src/pages/StudySetup/StudyTable.js
+++ web/src/pages/StudySetup/StudyTable.js
@@ -43,7 +43,8 @@ const DateCell = ({ row, column: { accessor } }) => {
   const rowValue = row[accessor];
   // const date = rowValue ? moment(rowValue).format("DD-MMM-YYYY") : "";
 
-  const date = rowValue && moment(rowValue).format("MMMM Do YYYY, h:mm:ss A");
+  // const date = rowValue && moment(rowValue).format("MMMM Do YYYY, h:mm:ss A");
+  const date = rowValue && moment(rowValue).format("DD-MMMM-YYYY h:mm:ss A");
   return <span>{date}</span>;
 };
 
